120c0fbb0c9f4f133924fc4115aa89a3530e9d0a
Shekhar <shekhar.s@inspiria.edu.in>
2020-03-28 21:54:10 +0530

    completed welcome page

--- lib/screens/welcome_screen.dart
+++ lib/screens/welcome_screen.dart
@@ -7,14 +7,19 @@ class WelcomePage extends StatelessWidget {
     return Scaffold(
       body: Center(
         child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Icon(
               Icons.hotel,
               size: 80,
             ),
-            Text(
-              '${AppConstants.appName}!',
-              style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+            Padding(
+              padding: const EdgeInsets.only(top: 20.0),
+              child: Text(
+                'Welcome to ${AppConstants.appName}!',
+                style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                textAlign: TextAlign.center,
+              ),
             )
           ],
         ),
